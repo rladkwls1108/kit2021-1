5485e5ebeaef352cf8feb4d83d8ea807e0334444
kim <rladkwls1108@gmail.com>
2021-03-30 16:47:01 +0900

    반복문 추가 리스트 고웁

---
+++ test.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+    #print("hello world")
+    #print("자몽")
+
+    block = ["ㄹ", "ㄱ","ㄴ", "ㅁ", "ㅣ", "ㅜ", "ㅗ"]
+
+    #print(block[0])
+    #print(block[1])
+    #print(block[2])
+    #print(block[3])
+    #print(block[4])
+
+    print(block)
+    print("블록 갯수:" , len(block))
+    for i in range(len(block)):
+        print(block[i])
